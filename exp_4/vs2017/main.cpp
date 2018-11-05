12e7cc8994eaed3d4b63e401dafb8178f3f31bd8
Feng12345 <2249557834@qq.com>
2018-11-05 23:24:02 +0800

    exp_4/vs2017

---
+++ exp_4/vs2017/main.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+using namespace std;
+#include "./gdal/gdal_priv.h"
+#pragma comment(lib, "gdal_i.lib")
+
+void inputIMG(int imgXlen, int imgYlen, int bandNum, int array, double buffArray[][5], GDALDataset* poSrcDS, GDALDataset* poDstDS, GByte* buffTmp, GByte buff[][5])
+{
+	// 数组求和double类型
+	double sum[1] = { 0.0 };
+	// 数组求和GByte类型
+	GByte pre[1] = { 0 };
+	// 多个for循环嵌套
+	for (int i = array / 2; i < imgYlen - array / 2; ++i)
+	{
+		for (int j = array / 2; j < imgXlen - array / 2; ++j)
+		{
+			for (int k = 0; k < bandNum; ++k)
+			{
+				sum[0] = 0.0;
+				// for循环求和---每个像素点的像素值
+				for (int a = 0; a < array; ++a)
+				{
+					for (int b = 0; b < array; ++b)
+					{
+						//读取数组的像素点
+						poSrcDS->GetRasterBand(k + 1)->RasterIO(GF_Read,
+							i + a - array / 2, j + b - array / 2, 1, 1, buffTmp, 1, 1, GDT_Byte, 0, 0);
+						buff[a][b] = buffTmp[0];
+						sum[0] += (double)buff[a][b] * buffArray[a][b];
+						//cout << buff[a][b] << "   " << sum[0] << endl;
+					}
+				}
+
+				pre[0] = (GByte)sum[0];
+				poDstDS->GetRasterBand(k + 1)->RasterIO(GF_Write,
+					i, j, 1, 1, pre, 1, 1, GDT_Byte, 0, 0);
+			}
+		}
+	}
+}
+
+
+int main()
+{
+	// 输入头像
+	GDALDataset* poSrcDS;
+	// 输出头像
+	GDALDataset* poDstDS_1;
+	GDALDataset* poDstDS_2;
+	GDALDataset* poDstDS_3;
+	GDALDataset* poDstDS_4;
+	GDALDataset* poDstDS_5;
+	GDALDataset* poDstDS_6;
+	// 图像的宽度和高度
+	int imgXlen, imgYlen;
+	// 输入图像路径
+	char* srcPath = "lena.jpg";
+	// 输出图像路径----卷积核一
+	char* dstPath_1 = "newlena1.tif";
+	char* dstPath_2 = "newlena2.tif";
+	char* dstPath_3 = "newlena3.tif";
+	char* dstPath_4 = "newlena4.tif";
+	char* dstPath_5 = "newlena5.tif";
+	char* dstPath_6 = "newlena6.tif";
+	// 图像内存存储
+	GByte* buffTmp;
+	// 图像波段数
+	int bandNum;
+
+	// 注册驱动
+	GDALAllRegister();
+
+	// 打开图像
+	poSrcDS = (GDALDataset*)GDALOpenShared(srcPath, GA_ReadOnly);
+
+	// 获取图像宽度，高度和波段数量
+	imgXlen = poSrcDS->GetRasterXSize();
+	imgYlen = poSrcDS->GetRasterYSize();
+	bandNum = poSrcDS->GetRasterCount();
+	// 输出获取的结果
+	cout << " Image X Length: " << imgXlen << endl;
+	cout << " Image Y Length: " << imgYlen << endl;
+	cout << " Band number :   " << bandNum << endl;
+
+	// 创建输出图像
+	poDstDS_1 = GetGDALDriverManager()->GetDriverByName("GTiff")->Create(
+		dstPath_1, imgXlen, imgYlen, bandNum, GDT_Byte, NULL);
+	poDstDS_2 = GetGDALDriverManager()->GetDriverByName("GTiff")->Create(
+		dstPath_2, imgXlen, imgYlen, bandNum, GDT_Byte, NULL);
+	poDstDS_3 = GetGDALDriverManager()->GetDriverByName("GTiff")->Create(
+		dstPath_3, imgXlen, imgYlen, bandNum, GDT_Byte, NULL);
+	poDstDS_4 = GetGDALDriverManager()->GetDriverByName("GTiff")->Create(
+		dstPath_4, imgXlen, imgYlen, bandNum, GDT_Byte, NULL);
+	poDstDS_5 = GetGDALDriverManager()->GetDriverByName("GTiff")->Create(
+		dstPath_5, imgXlen, imgYlen, bandNum, GDT_Byte, NULL);
+	poDstDS_6 = GetGDALDriverManager()->GetDriverByName("GTiff")->Create(
+		dstPath_6, imgXlen, imgYlen, bandNum, GDT_Byte, NULL);
+
+	// 根据图像的宽度和高度分配内存
+	buffTmp = (GByte*)CPLMalloc(imgXlen*imgYlen * sizeof(GByte));
+	// 卷积核数组，记录数组维度
+	double buffArray_1[5][5] = {
+		0, 1 * 0.2, 0, 0, 0,
+		1 * 0.2, 1 * 0.2, 1 * 0.2, 0, 0,
+		0, 1 * 0.2, 0, 0, 0
+	};
+	int array_1 = 3;
+	double buffArray_2[5][5] = {
+		1 * 0.2, 0, 0, 0, 0,
+		0, 1 * 0.2, 0, 0 ,0,
+		0, 0, 1 * 0.2, 0, 0,
+		0, 0, 0, 1 * 0.2, 0,
+		0, 0, 0, 0, 1 * 0.2
+	};
+	int array_2 = 5;
+	double buffArray_3[5][5] = {
+		-1, -1, -1, 0, 0,
+		-1, 8, -1, 0, 0,
+		-1, -1, -1, 0, 0
+	};
+	int array_3 = 3;
+	double buffArray_4[5][5] = {
+		-1, -1, -1, 0, 0,
+		-1, 9, -1, 0, 0,
+		-1, -1, -1, 0, 0
+	};
+	int array_4 = 3;
+	double buffArray_5[5][5] = {
+		-1, -1, 0, 0, 0,
+		-1, 0, -1, 0, 0,
+		0, -1, -1, 0, 0
+	};
+	int array_5 = 3;
+	double buffArray_6[5][5] = {
+		0.0120 / 25, 0.1253 / 25, 0.2736 / 25, 0.1253 / 25, 0.0120 / 25,
+		0.1253 / 25, 1.3054 / 25, 2.8514 / 25, 1.3054 / 25, 0.1253 / 25,
+		0.2736 / 25, 2.8514 / 25, 6.2279 / 25, 2.8514 / 25, 0.2736 / 25,
+		0.1253 / 25, 1.3054 / 25, 2.8514 / 25, 1.3054 / 25, 0.1253 / 25,
+		0.0120 / 25, 0.1253 / 25, 0.2736 / 25, 0.1253 / 25, 0.0120 / 25
+	};
+	int array_6 = 5;
+	// 开创数组用于存储像素点，方便后面数组乘机
+	GByte buff[5][5];
+
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < 5; ++j)
+		{
+			buff[i][j] = 0;
+		}
+	}
+
+	// 函数调用
+	inputIMG(imgXlen, imgYlen, bandNum, array_1, buffArray_1, poSrcDS, poDstDS_1, buffTmp, buff);
+	cout << "卷积核__1......" << endl;
+	inputIMG(imgXlen, imgYlen, bandNum, array_2, buffArray_2, poSrcDS, poDstDS_2, buffTmp, buff);
+	cout << "卷积核__2......" << endl;
+	inputIMG(imgXlen, imgYlen, bandNum, array_3, buffArray_3, poSrcDS, poDstDS_3, buffTmp, buff);
+	cout << "卷积核__3......" << endl;
+	inputIMG(imgXlen, imgYlen, bandNum, array_4, buffArray_4, poSrcDS, poDstDS_4, buffTmp, buff);
+	cout << "卷积核__4......" << endl;
+	inputIMG(imgXlen, imgYlen, bandNum, array_5, buffArray_5, poSrcDS, poDstDS_5, buffTmp, buff);
+	cout << "卷积核__5......" << endl;
+	inputIMG(imgXlen, imgYlen, bandNum, array_6, buffArray_6, poSrcDS, poDstDS_6, buffTmp, buff);
+	cout << "卷积核__6......" << endl;
+	// 清除内存
+	CPLFree(buffTmp);
+	// 关闭dataset
+	GDALClose(poDstDS_1);
+	GDALClose(poDstDS_2);
+	GDALClose(poDstDS_3);
+	GDALClose(poDstDS_4);
+	GDALClose(poDstDS_5);
+	GDALClose(poDstDS_6);
+	GDALClose(poSrcDS);
+
+	system("PAUSE");
+	return 0;
+}
